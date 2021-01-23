8bd16b682565dfc048bcb7739c3d00ebe9894241
dsaperov <dsaperov.md@gmail.com>
2021-01-23 23:15:31 +0300

    h/w for lesson 7 done

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,121 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+class Water:
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            res = Storm()
+
+        elif isinstance(other, Fire):
+            res = Steam()
+
+        elif isinstance(other, Earth):
+            res = Mud()
+
+        else:
+            res = None
+
+        return res
+
+
+class Air:
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            res = Lightning()
+
+        elif isinstance(other, Earth):
+            res = Dust()
+
+        else:
+            res = None
+
+        return res
+
+
+class Fire:
+    def __str__(self):
+        return 'Огонь'
+
+    def __add__(self, other):
+        if isinstance(other, Earth):
+            res = Lava()
+
+        else:
+            res = None
+
+        return res
+
+
+class Earth:
+    def __str__(self):
+        return 'Земля'
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+
+class Mud:
+    def __str__(self):
+        return 'Грязь'
+
+
+class Lightning:
+    def __str__(self):
+        return 'Молния'
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+
+class Lava:
+    def __str__(self):
+        return 'Лава'
+
+
+transformations = ((Water(), Air()),
+                   (Water(), Fire()),
+                   (Water(), Earth()),
+                   (Air(), Fire()),
+                   (Air(), Earth()),
+                   (Fire(), Earth())
+                   )
+
+for transformation in transformations:
+    el1 = transformation[0]
+    el2 = transformation[1]
+    print(el1, '+', el2, '=', el1 + el2)
+
+
+#   Вода + Воздух = Шторм
+#   Вода + Огонь = Пар
+#   Вода + Земля = Грязь
+#   Воздух + Огонь = Молния
+#   Воздух + Земля = Пыль
+#   Огонь + Земля = Лава
+
+# Сложение элементов реализовывать через __add__
+# Если результат не определен - то возвращать None
+# Вывод элемента на консоль реализовывать через __str__
+#
+# Примеры преобразований:
+#   print(Water(), '+', Air(), '=', Water() + Air())
+#   print(Fire(), '+', Air(), '=', Fire() + Air())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from random import randint
+from termcolor import cprint
+
 
 # Доработать практическую часть урока lesson_007/python_snippets/08_practice.py
 
@@ -25,7 +27,150 @@ from random import randint
 
 # Человеку и коту надо вместе прожить 365 дней.
 
-# TODO здесь ваш код
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.home = None
+        self.fullness = 30
+
+    def __str__(self):
+        res = 'Кот {} сытый на {}.'.format(self.name, self.fullness)
+        return res
+
+    def eat(self):
+        self.fullness += 20
+        self.home.cat_food -= 10
+        cprint('Кот {} обожрался!'.format(self.name), color='magenta')
+
+    def sleep(self):
+        self.fullness -= 10
+        cprint('Кот {} спал весь день!'.format(self.name), color='magenta')
+
+    def rip_off_wallpapers(self):
+        self.fullness -= 10
+        self.home.mud += 5
+        cprint('Кот {} от скуки сгрыз обои!'.format(self.name), color='magenta')
+
+    def act(self):
+        if self.fullness < 30 and self.home.cat_food >= 10:
+            self.eat()
+        else:
+            if self.fullness < 30 and self.home.cat_food < 10:
+                cprint('В доме закончилась кошачья еда - кот {} голодает!'.format(self.name), color='red')
+            dice = randint(1, 2)
+            if dice == 1:
+                self.sleep()
+            else:
+                self.rip_off_wallpapers()
+
+        if self.fullness <= 0:
+            self.home.alive_citizens.remove(self)
+            cprint('Кот {} умер голодной смертью...'.format(self.name), color='red')
+
+
+class Human:
+    def __init__(self, name, home):
+        self.name = name
+        self.home = home
+        self.home.alive_citizens.append(self)
+        self.money = 100
+        self.fullness = 100
+
+    def __str__(self):
+        res = '{} сытый на {}. Остаток денег - {}'.format(self.name, self.fullness, self.money)
+        return res
+
+    def pick_up_a_cat(self, _cat, home):
+        _cat.home = home
+        self.home.alive_citizens.append(_cat)
+        cprint('{} забрал домой кота и назвал его {}!'.format(self.name, _cat.name), color='magenta')
+
+    def buy_cat_food(self):
+        self.home.cat_food += 50
+        self.money -= 50
+        cprint('{} купил еду котам!'.format(self.name), color='magenta')
+
+    def buy_human_food(self):
+        self.home.human_food += 50
+        self.money -= 50
+        cprint('{} купил еду себе!'.format(self.name), color='magenta')
+
+    def clean_home(self):
+        self.home.mud -= 100
+        self.fullness -= 20
+        cprint('{} поклеил обои и очистил дом от шерсти!'.format(self.name), color='magenta')
+
+    def eat(self):
+        self.fullness += 40
+        cprint('{} поел!'.format(self.name), color='magenta')
+
+    def work(self):
+        self.money += 150
+        self.fullness -= 20
+        cprint('{} поработал!'.format(self.name), color='magenta')
+
+    def have_a_rest(self):
+        self.money -= 20
+        cprint('{} отдохнул!'.format(self.name), color='magenta')
+
+    def act(self):
+        if self.fullness <= 20:
+            self.eat()
+        elif self.home.human_food < 50:
+            self.buy_human_food()
+        elif self.money < 50:
+            self.work()
+        elif self.home.cat_food < 60:
+            self.buy_cat_food()
+        elif self.home.mud > 100:
+            self.clean_home()
+        else:
+            self.have_a_rest()
+
+        if self.fullness <= 0:
+            self.home.alive_citizens.remove(self)
+            cprint('{} умер голодной смертью...'.format(self.name), color='red')
+
+
+class Home:
+    def __init__(self):
+        self.human_food = 100
+        self.cat_food = 0
+        self.mud = 0
+        self.alive_citizens = []
+
+    def __str__(self):
+        res = 'Осталось {} единиц человеческой еды, {} единиц кошачей еды. Загрязненность - {}.'.format(self.human_food,
+                                                                                                        self.cat_food,
+                                                                                                        self.mud)
+        return res
+
+    def act(self):
+        if self.mud >= 250:
+            self.alive_citizens.clear()
+            cprint('Жители дома задохнулись от пыли и шерсти...', color='red')
+
+
+sweet_home = Home()
+human1 = Human('Василий', sweet_home)
+
+cats = (Cat('Песик'), Cat('Динозавр'), Cat('Вован'), Cat('Дональд'), Cat('Хайзенберг'), Cat('Цыпленок'),
+        Cat('Потрошитель'), Cat('Пушок'), Cat('Вредина'))
+
+for cat in cats:
+    human1.pick_up_a_cat(cat, sweet_home)
+
+for day in range(1, 366):
+    cprint('============== День {} =============='.format(day), color='green')
+    for citizen in sweet_home.alive_citizens:
+        citizen.act()
+    sweet_home.act()
+    if not sweet_home.alive_citizens:
+        break
+    cprint('-------------- На конец дня --------------'.format(day), color='blue')
+    for citizen in sweet_home.alive_citizens:
+        print(citizen)
+    print(sweet_home)
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
